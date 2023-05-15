d7a114df4cfad3d8277b517472e163e678741f01
njasi <nick@jasinski3.com>
2023-05-14 17:46:21 -0700

    this is a really long commit message to test the functionality of the hook i just created oops i forgot to check if there are other steps i need to do to setup the hook...

---
+++ prepare-commit-msg.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys, re
+from subprocess import check_output
+
+def format_commit(commit_message, line_first=50, line_other=72):
+    # clean out double spaces and extra newlines
+    commit_words = commit_message.replace("\n", " ").split(" ")
+    lines = []
+    line_len = line_first
+
+    while len(commit_words) > 0:
+        line = ""
+        while len(commit_words) > 0 and \
+               len(line) + len(commit_words[0]) + 1 <= line_len:
+            line += f" {commit_words[0]}"
+            del commit_words[0]
+        line_len = line_other
+        lines += [line]
+
+    if len(lines) > 1:
+        lines.insert(1,"")
+
+    return "\n".join(lines)
+
+def main(commit_msg_filepath):
+    print("Formatting your commit to the 50-70 rule")
+    with open(commit_msg_filepath, "r+") as file:
+        commit_message = file.read()
+        format_commit(commit_message)
+
+if __name__ == "__main__":
+    pass
+    commit_msg_filepath = sys.argv[1]
+    main(commit_msg_filepath)
